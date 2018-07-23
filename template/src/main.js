c3d574ccaf6316760f0f8665767409cc8ee499ee
kooritea <ochatokori@gmail.com>
2018-07-23 19:51:28 +0800

    fix main.js

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,12 @@
+{{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+{{/if_eq}}
 import Vue from 'vue'
 import App from './App'
+{{#router}}
 import router from './router'
+{{/router}}
 import store from './store'
 import axios from './axios'
 
@@ -11,8 +15,15 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   store,
+  {{#router}}
+  router,
+  {{/router}}
+  {{#if_eq build "runtime"}}
+  render: h => h(App)
+  {{/if_eq}}
+  {{#if_eq build "standalone"}}
   components: { App },
   template: '<App/>'
+  {{/if_eq}}
 })
