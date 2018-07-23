88f5930672b4ece6ff28c5b857bc218d965884e9
kooritea <ochatokori@gmail.com>
2018-07-23 19:24:27 +0800

    add vuex and axios

---
+++ template/src/axios/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+axios.interceptors.request.use(
+    config => {
+        return config;
+    },
+    err => {
+        return Promise.reject(err);
+    }
+);
+axios.interceptors.response.use(
+    res => {
+      return res
+    }
+);
